963960831a46c0fbb8a329144f9d0cff7aa43f65
Divyom Kumar <divyom.kumar@unilever.com>
2023-02-23 20:24:48 +0530

    added signup form skeleton

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,51 @@
-const Home = () => {
+import React, { useState, useEffect } from "react";
 
+
+
+const Home = () => {
+    const [userName, setUserName] = useState('')
+    console.log(userName)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const submitForm = async () => {
+        const formData = new FormData()
+        await formData.append('username', userName);
+        await formData.append('email', email);
+        await formData.append('password', password)
+
+        
+    }
+    return (
+        <>
+            <div>
+                <label for="fname">Username:</label><br />
+                <input
+                    onChange={async (e) => {
+                        await setUserName(e.target.value)
+                    }}
+                    type="text"
+                    id="fname"
+                    name="fname" /><br />
+                <label for="email">Email:</label><br />
+                <input
+                    onChange={async (e) => {
+                        await setEmail(e.target.value)
+                    }}
+                    type="text"
+                    id="emial"
+                    name="email"></input><br />
+                <label for="password">Password:</label><br />
+                <input
+                    onChange={async (e) => {
+                        await setPassword(e.target.value)
+                    }}
+                    type="password"
+                    id="password"
+                    name="password"></input>
+                <button onClick={submitForm}>Signup</button>
+            </div>
+        </>
+    )
 }
 
 
