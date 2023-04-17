10c746cd12c67ec48eb053f5199e956783cd49a6
Divyom Kumar <divyom.kumar@unilever.com>
2023-04-17 22:11:44 +0530

    Worked on Calender

--- src/components/calenderDetails.js
+++ src/components/calenderDetails.js
@@ -7,14 +7,37 @@ import axios from "axios";
 
 const CalenderDetail = () => {
     const [calData, setCalData ] = useState({})
-    
+    const currDate = new Date()
+    const currMonth = currDate.getMonth() + 1
+
     useEffect(() =>{
-        axios.get('http://127.0.0.1:8000/calenderapi/calender-by-month/?created_by=1&month_filter=4')
+        let url = 'http://127.0.0.1:8000/calenderapi/calender-by-month/?created_by=1&month_filter='
+        if (currMonth !== '' && currMonth !== undefined && currMonth !== '') url += currMonth
+        axios.get(url)
         .then((res) =>{
             setCalData(res.data)
             console.log(res.data)
         })
     },[])
+
+        // console.log(arg)
+        
+    const handleDatesSet =(arg) =>{
+        if (arg.view.type === 'dayGridMonth'){
+            const nextDate = new Date(arg.endStr)
+            const nextMonth = nextDate.getMonth()
+            const prevDate = new Date(arg.startStr)
+            const prevMonth = prevDate.getMonth()
+            let url = 'http://127.0.0.1:8000/calenderapi/calender-by-month/?created_by=1&month_filter='
+            if (nextMonth !== '' && nextMonth !== undefined && nextMonth !== '') url += nextMonth
+            // if (prevMonth !== '' && prevMonth !== undefined && prevMonth !== '') url += prevMonth
+            axios.get(url)
+            .then((res) =>{
+                setCalData(res.data)
+            })
+            // console.log(endDate)
+        }
+    }
     // const events = [
     //     {
     //         title: 'Event 1',
@@ -40,10 +63,11 @@ const CalenderDetail = () => {
                     plugins={[dayGridPlugin]}
                     initialView="dayGridMonth"
                     events={calData}
+                    datesSet={handleDatesSet}
                     height="600px"
                 />
 
-
+                
             </Container>
 
         </>
